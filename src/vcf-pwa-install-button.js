c0bd98331e47d501ce2daf0ad47982cf88994637
Sohrab Taee <sohrab.taee@gmail.com>
2019-08-21 11:04:22 +0300

    Changed the version of vaadin-button to be compatible with TaskMob.

--- src/vcf-pwa-install-button.js
+++ src/vcf-pwa-install-button.js
@@ -27,7 +27,7 @@ class VcfPwaInstallButton extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '0.1.1';
+    return '0.1.2';
   }
 
   static get properties() {
